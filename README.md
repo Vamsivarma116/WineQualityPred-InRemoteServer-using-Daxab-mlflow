6603d30ff2c34fda756adc3e68c097299e150e80
VamsiVarma <vamsi.hansi@gmail.com>
2024-06-01 02:02:15 +0530

    2nd mlflow dugshub commit

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # WineQualityPred-InRemoteServer-using-Daxab-mlflow
 
-# Dagshub
+# Allowing Dagshub to track my repo MLflow
+============================================
 
-MLFLOW_TRACKING_URI=https://dagshub.com/entbappy/chest-Disease-Classification-MLflow-DVC.mlflow
-MLFLOW_TRACKING_USERNAME=entbappy
-MLFLOW_TRACKING_PASSWORD=6824692c47a4545eac5b10041d5c8edbcef0
-python script.py
+# Method 1
+===========
+# Step 1 =: Installing the dagshub in to our current working repo or directory
 
+```bash
+pip install -q dagshub mlflow
+```
+# Step 2 =: Use the DagsHub client to setup connection information for MLflow
+
+```bash
+import dagshub
+dagshub.init(repo_owner='Vamsivarma116', repo_name='WineQualityPred-InRemoteServer-using-Daxab-mlflow', mlflow=True)
+```
+# Step 3 =: Use MLflow to log params and metrics(Which is already done in WineQualityPred.py file line no 74 - 78)
+
+```bash
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+```
+
+# Method : 2  
+=============
 # Run this to export as env variables:
 
 ```bash
--- WineQualityPred.py
+++ WineQualityPred.py
@@ -16,6 +16,9 @@ from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
+import dagshub
+
+dagshub.init(repo_owner='Vamsivarma116', repo_name='WineQualityPred-InRemoteServer-using-Daxab-mlflow', mlflow=True)
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -74,8 +77,6 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        predictions = lr.predict(train_x)
-        signature = infer_signature(train_x, predictions)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
@@ -86,7 +87,6 @@ if __name__ == "__main__":
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
-            )
+                lr, "model", registered_model_name="ElasticnetWineModel")
         else:
-            mlflow.sklearn.log_model(lr, "model", signature=signature) 
+            mlflow.sklearn.log_model(lr, "model") 
